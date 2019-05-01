03d41303b26ec127fac2cf80a7f975d32cd8083b
Eraz <48658740+SirEraz@users.noreply.github.com>
2019-05-01 11:17:47 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+def bookISBN(isbn):
+# url = str(“https://isbnsearch.org/isbn/“ + isbn)
+# source = requests.get(url)
+# text = source.text
+# soup = BeautifulSoup(text, “html.parser”)
+
+# find title 
+# find author 
+# find publisher 
+# find publication date 
